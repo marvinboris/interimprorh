ee8f7acbf579e93e878295093e872d134b8bb359
Brainer21 <jarisl.ultio.21@gmail.com>
2024-06-17 09:39:44 +0100

    Optimized app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="/favicon.webp">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://maps.google.com">
+    <link rel="preconnect" href="https://cdn.jsdelivr.net">
 
     <title>Interim Pro RH</title>
     <meta name="description"
